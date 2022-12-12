4991aa4e09120864f655e9893ccf31c477dc5385
Nathálya Ricci <nathalyaricci@gmail.com>
2022-12-12 17:52:15 -0300

    adiciona exercício 05

---
+++ exercicios-javascript/lista-de-exercicio_05-funcao.js
@@ -0,0 +1,32 @@
+/*
+  Utilizando funções, elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+  Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida a efetuar o cálculo adequado.
+
+  Código Condição de pagamento:
+
+  1- À vista no débito, recebe 10% de desconto;
+  2- À vista no dinheiro ou PIX, recebe 15% de desconto;
+  3- Em 2x, preço normal de etiqueta sem juros;
+  4- Acima de 3x, preço normal de etiqueta mais juros de 10%.
+*/
+
+const precoEtiqueta = 200;
+const formaDePagamento = 4;
+
+function aplicarDesconto(valor, desconto) {
+	return valor - valor * (desconto / 100);
+}
+
+function aplicarJuros(valor, juros) {
+	return valor + valor * (juros / 100);
+}
+
+if (formaDePagamento === 1) {
+	console.log(aplicarDesconto(precoEtiqueta, 10));
+} else if (formaDePagamento === 2) {
+	console.log(aplicarDesconto(precoEtiqueta, 15));
+} else if (formaDePagamento === 3) {
+	console.log(precoEtiqueta);
+} else {
+	console.log(aplicarJuros(precoEtiqueta, 10));
+}
