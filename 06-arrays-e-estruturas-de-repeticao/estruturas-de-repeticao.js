c5c91ead2e1efc5aa6c932577bdf13a0ef817892
Nathálya Ricci <nathalyaricci@gmail.com>
2022-12-14 15:29:16 -0300

    adiciona aula estruturas de repetição

---
+++ 06-arrays-e-estruturas-de-repeticao/estruturas-de-repeticao.js
@@ -0,0 +1,8 @@
+const nome = "Lya Ricci"; // obs.: uma string é um array de caracteres
+
+console.log(nome.length);
+
+for (let i = 0; i < nome.length; i++) {
+	const letra = nome[i];
+	console.log(letra);
+}
